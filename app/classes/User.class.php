b14d677c187e62014b3ebb1ec8c9cec0d25f4fb0
Mussab ElDash <mussab14@gmail.com>
2014-09-22 17:36:17 +0300

    login done

---
+++ app/classes/User.class.php
@@ -0,0 +1,56 @@
+<?php
+//User.class.php
+require_once 'DB.class.php';
+
+
+class User {
+
+	public $id;
+	public $username;
+	public $firstname;
+	public $lastname;
+	public $hashedPassword;
+	public $email;
+	public $avatar;
+
+	//Constructor is called whenever a new object is created.
+	//Takes an associative array with the DB row as an argument.
+	function __construct($data) {
+		$this->id = (isset($data['id'])) ? $data['id'] : "";
+		$this->username = (isset($data['user_name'])) ? $data['user_name'] : "";
+		$this->hashedPassword = (isset($data['password'])) ? $data['password'] : "";
+		$this->email = (isset($data['email'])) ? $data['email'] : "";
+		$this->avatar = (isset($data['avatar'])) ? $data['avatar'] : "";
+		$this->firstname = (isset($data['first_name'])) ? $data['first_name'] : "";
+		$this->lastname = (isset($data['last_name'])) ? $data['last_name'] : "";
+	}
+
+	public function save($isNewUser = false) {
+		//create a new database object.
+		$db = new DB();
+
+		//set the data array
+		$data = array(
+			"user_name" => "'$this->username'",
+			"first_name" => "'$this->firstname'",
+			"last_name" => "'$this->lastname'",
+			"password" => "'$this->hashedPassword'",
+			"email" => "'$this->email'",
+			"avatar" => "'$this->avatar'"
+		);
+		//if the user is already registered and we're
+		//just updating their info.
+		if(!$isNewUser) {
+			//update the row in the database
+			$db->update($data, 'users', 'id = '.$this->id);
+		}else {
+			//if the user is being registered for the first time.
+			$this->id = $db->insert($data, 'users');
+			$this->joinDate = time();
+		}
+		return true;
+	}
+	
+}
+
+?>
---
+++ app/index.php
@@ -0,0 +1,62 @@
+<?php
+	require_once 'includes/global.inc.php';
+	$error = "";
+	$username = "";
+	$password = "";
+	//check to see if they've submitted the login form
+	if(isset($_POST['submit-login'])) { 
+
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+		$userTools = new UserTools();
+		if(!$userTools->login($username, $password)){
+			$error = "Incorrect username or password. Please try again.";
+		}
+	}
+	$logged_in = false;
+	$user;
+	//check to see if they're logged in
+	if(isset($_SESSION['logged_in'])) {
+		$logged_in = true;
+		//get the user object from the session
+		$user = unserialize($_SESSION['user']);
+	}
+?>
+<html>
+	<head>
+		<title>eShop</title>
+		<link rel="stylesheet" type="text/css" href="css/credentials.css"/>
+		<link rel="stylesheet" type="text/css" href="css/all.css"/>
+	</head>
+	<body>
+		<div class="container">
+			<h1>eShop</h1>
+			<div class="pull-right">
+				<?php
+					echo $error;
+					if ($logged_in) {
+						echo "
+						<img src='$user->avatar' height=25 width=25/>
+						<a href='edit.php'>$user->firstname $user->lastname</a><br/>
+						<a href='logout.php'>Logout</a>
+						";
+					}else{
+						echo "
+						<form action='index.php' method='POST' id='credentials'>
+							<label for='usernameform'>User Name :</label>
+							<input type='text' name='username' id='usernameform'/>
+							<label for='passwordform'>Password :</label>
+							<input type='text' name='password' id='passwordform'/>
+							<input type='submit' name='submit-login'value='Login'/>
+						</form>
+						<a href='register.php'>Register</a>
+						";
+					}
+				?>
+			</div>
+		</div>
+		<hr/>
+	</body>
+</html>
+<?php $db->close() ?>
