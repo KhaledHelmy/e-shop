f375e532f6b2d5e54e9ff4b57963a53fc411dc36
System Administrator <root@Omars-MacBook-Pro-2.local>
2014-09-27 05:09:23 +0200

    Added database update and redirect to index after checking out

--- app/confirmation.php
+++ app/confirmation.php
@@ -2,14 +2,33 @@
 	require_once 'includes/global.inc.php';
 	$logged_in = false;
 	$user;
+
+	//check that product id exists
+	if(!isset($_POST['product_id'])) {
+		header("Location: index.php");
+	}
+
 	//check to see if they're logged in
-	if(!isset($_SESSION['logged_in']) || !isset($_POST['product_id'])) {
+	if(!isset($_SESSION['logged_in'])) {
 		header("Location: index.php");
 	}
 	else {
 		$logged_in = true;
 		//get the user object from the session
 		$user = unserialize($_SESSION['user']);
+	}
+
+	//check product exists and is in stock
+	$result = mysql_query("SELECT * FROM products WHERE id = " . $_POST['product_id']);
+	if(!($product = mysql_fetch_array($result)) || $product['stock'] < 1)
+		header("Location: index.php");
+
+	//check if the user already checked out the product
+	if(isset($_POST['bought'])) {
+		mysql_query("INSERT INTO transactions (user_id, product_id) VALUES (" . $user->id . ", " . $product['id'] . ")");
+		$new_stock = $product['stock'] - 1;
+		mysql_query("UPDATE products SET stock=" . $new_stock . " WHERE id=" . $product['id']);
+		header("Location: index.php");
 	}
 ?>
 <html>
@@ -29,8 +48,9 @@
 						<p>$row[title], $row[price] EGP</p>
 						<hr>
 						<div><strong>Total $row[price] EGP</strong></div>
-						<form method='POST'>
-							<input type='submit' value='Checkout' />
+						<form action='confirmation.php' method='POST'>
+							<input type='submit' name='bought' value='Checkout' />
+							<input type='hidden' name='product_id' value='$row[id]'>
 						</form>
 					";
 				}
